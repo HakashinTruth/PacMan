9cbfc3a563364b2f615cec430144d321b5205272
HakashinTruth <regiongamer11@gmail.com>
2025-03-18 13:04:52 +0000

    test

--- Scripts/Points.py
+++ Scripts/Points.py
@@ -11,7 +11,6 @@ class Point:
         self.vel = Vector(0, 0)
         self.spriteimgs = Spritesheet(r"C:\Users\Student\OneDrive\Desktop\project\PacMan\pacmanPack\CoinTransparent.png", rows, columns)
         self.radius = 3 
- 
         # Input rate control variables
         self.input_frame_counter = 0
         self.main_fps = 60  # Assuming main game runs at 60fps
@@ -26,4 +25,7 @@ class Point:
         
     def next_frame(self):
         self.spriteimgs.next_frame()
+    
+    def pointCollision(self):
+        
          
